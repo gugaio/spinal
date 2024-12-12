2c9720543e2a6b4d0695130bc19b1dca05b98231
gugaio <gustavo.barros@jurus.com.br>
2024-12-11 22:00:35 -0300

    Update CORS Access-Control-Allow-Origin

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ controller = SpinalController()
 
 @app.route('/ping', methods=['GET'])
 def ping():
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return jsonify({'message': 'Pong!'}), 200
 
 @app.route('/message', methods=['POST'])
@@ -22,10 +23,18 @@ def message():
         messages = data['messages']
         tools = data['tools'] if 'tools' in data else None
         responseMessage = controller.handle(messages, tools)
+        response.headers.add('Access-Control-Allow-Origin', '*')
         return jsonify({'message': responseMessage}), 200
     else:
         return jsonify({'error': 'No message attribute found'}), 400
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+    
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
